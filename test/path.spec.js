b5b425dd53926d8cc0aa25fb50d2026b93418a19
marycielopb <mparionabernaola@gmail.com>
2020-11-23 23:22:22 -0500

    validate path and absolute path test closes #7

---
+++ test/path.spec.js
@@ -0,0 +1,30 @@
+const { validatePath, validateAbsolutePath } = require('../src/path.js');
+
+const relativePath = '.\\README.md';
+const absolutePath = 'E:\\BOOTCAMP\\PROJECTS\\MD-LINKS\\LIM013-fe-md-links';
+const relativePathConverted = 'E:\\BOOTCAMP\\PROJECTS\\MD-LINKS\\LIM013-fe-md-links\\README.md';
+const invalidPath = 'C:\\BOOTCAMP';
+
+describe('validate path', () => {
+  test('is a function', () => {
+    expect(typeof validatePath).toBe('function');
+  });
+  test('the given path is valid', () => {
+    expect(validatePath(relativePath)).toBe(true);
+  });
+  test('the given path is invalid or does not exist', () => {
+    expect(validatePath(invalidPath)).toBe(false);
+  });
+});
+
+describe('validate absolute path', () => {
+  test('is a function', () => {
+    expect(typeof validateAbsolutePath).toBe('function');
+  });
+  test('the given path is absolute', () => {
+    expect(validateAbsolutePath(absolutePath)).toEqual(absolutePath);
+  });
+  test('the given path is relative and is converted to absolute', () => {
+    expect(validateAbsolutePath(relativePath)).toEqual(relativePathConverted);
+  });
+});
