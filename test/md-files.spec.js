31b84154a86787486e1104f37468527d12590fa6
marycielopb <mparionabernaola@gmail.com>
2020-11-29 23:06:43 -0500

    test 100%: validate path, get md files, get md links and pure functions - closes #13

---
+++ test/md-files.spec.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const {
+  isDirectory, readDirectory, isFile, isMd, getMdFiles,
+} = require('../src/md-links-list/md-files');
+
+const cwd = process.cwd();
+const relativeDirectory = '.\\test-files';
+const relativeMdFile = '.\\test-files\\test-links-1.md';
+const absoluteDirectory = path.join(cwd, '\\test-files');
+const absoluteMdFile = path.join(cwd, '\\test-files\\test-links-1.md');
+const absoluteNoMdFile = path.join(cwd, '\\test-files\\test-js-1.js');
+const relativeNoMdFile = '\\test-files\\test-js-1.0.js';
+
+describe('validate directory path', () => {
+  test('it is a function', () => {
+    expect(typeof isDirectory).toBe('function');
+  });
+  test('the given path points to a directory', () => {
+    expect(isDirectory(absoluteDirectory)).toBe(true);
+    expect(isDirectory(relativeDirectory)).toBe(true);
+  });
+  test('the given path is absolute and does not point a directory', () => {
+    expect(isDirectory(absoluteMdFile)).toBe(false);
+    expect(isDirectory(relativeMdFile)).toBe(false);
+  });
+});
+
+describe('read directory', () => {
+  test('it is a function', () => {
+    expect(typeof readDirectory).toBe('function');
+  });
+  test('it returns a files array', () => {
+    expect(readDirectory(absoluteDirectory)).toEqual([
+      'more-test-files',
+      'test-empty.md',
+      'test-links-1.md',
+      'test-links-2.md']);
+  });
+});
+
+describe('validate file path', () => {
+  test('it is a function', () => {
+    expect(typeof isFile).toBe('function');
+  });
+  test('the given path points to a file', () => {
+    expect(isFile(absoluteMdFile)).toBe(true);
+    expect(isFile(relativeMdFile)).toBe(true);
+  });
+  test('the given path does not point a file', () => {
+    expect(isFile(absoluteDirectory)).toBe(false);
+    expect(isFile(relativeDirectory)).toBe(false);
+  });
+});
+
+describe('validate .md file', () => {
+  test('it is a function', () => {
+    expect(typeof isMd).toBe('function');
+  });
+  test('the given path points to a file that has a .md extension', () => {
+    expect(isMd(absoluteMdFile)).toBe(true);
+    expect(isMd(relativeMdFile)).toBe(true);
+  });
+  test('the given path does point to a file that has not a .md extension', () => {
+    expect(isMd(absoluteNoMdFile)).toBe(false);
+    expect(isMd(relativeNoMdFile)).toBe(false);
+  });
+});
+
+describe('get .md files', () => {
+  test('it is a function', () => {
+    expect(typeof getMdFiles).toBe('function');
+  });
+  test('it returns a .md files array', () => {
+    expect(getMdFiles(absoluteDirectory)).toEqual([
+      `${path.join(cwd, '\\test-files\\test-empty.md')}`,
+      `${path.join(cwd, '\\test-files\\test-links-1.md')}`,
+      `${path.join(cwd, '\\test-files\\test-links-2.md')}`,
+    ]);
+  });
+});
---
+++ test/validate-path.spec.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const {
+  isPath, isAbsolute, convertToAbsolutePath, isPathStr, validatePath,
+} = require('../src/md-links-list/validate-path.js');
+
+const cwd = process.cwd();
+const bufferPath = Buffer.from('.\\test-files\\test-links-1.md', 'utf-8');
+const relativePath = '.\\test-files\\test-links-1.md';
+const absolutePath = path.join(cwd, relativePath);
+const absolutePathDoesNottExist = 'C:\\absolute-path';
+const relativePathDoesNotExist = '.\\relative-path';
+
+describe('validate path exists', () => {
+  test('is a function', () => {
+    expect(typeof isPath).toBe('function');
+  });
+  test('the given path exists and is valid', () => {
+    expect(isPath(relativePath)).toBe(true);
+    expect(isPath(bufferPath)).toBe(true);
+  });
+  test('the given path is invalid or does not exist', () => {
+    expect(isPath(relativePathDoesNotExist)).toBe(false);
+    expect(isPath('')).toBe(false);
+  });
+});
+
+describe('validate absolute path', () => {
+  test('it is a function', () => {
+    expect(typeof isAbsolute).toBe('function');
+  });
+  test('the given path is absolute', () => {
+    expect(isAbsolute(absolutePath)).toBe(true);
+    expect(isAbsolute(absolutePathDoesNottExist)).toBe(true);
+  });
+  test('the given path is not absolute', () => {
+    expect(isAbsolute(relativePath)).toBe(false);
+    expect(isAbsolute(relativePathDoesNotExist)).toBe(false);
+  });
+});
+
+describe('convert to absolute path', () => {
+  test('it is a function', () => {
+    expect(typeof convertToAbsolutePath).toBe('function');
+  });
+  test('the given path is relative and is converted to absolute', () => {
+    expect(convertToAbsolutePath(relativePath)).toBe(absolutePath);
+  });
+});
+
+describe('validate path exists and is a string', () => {
+  test('it is a function', () => {
+    expect(typeof isPathStr).toBe('function');
+  });
+  test('the given path exists and is a string', () => {
+    expect(isPathStr(relativePath)).toBe(true);
+    expect(isPathStr(absolutePath)).toBe(true);
+  });
+  test('the given path exists but is not a string', () => {
+    expect(isPathStr(bufferPath)).toBe(false);
+  });
+  test('the given path is invalid or does not exist', () => {
+    expect(isPathStr(relativePathDoesNotExist)).toBe(false);
+    expect(isPathStr('')).toBe(false);
+  });
+});
+describe('validate path', () => {
+  test('is is a function', () => {
+    expect(typeof validatePath).toBe('function');
+  });
+  test('the given path is absolute', () => {
+    expect(validatePath(absolutePath)).toBe(absolutePath);
+  });
+  test('the given path is relative and is converted to absolute', () => {
+    expect(validatePath(relativePath)).toBe(absolutePath);
+  });
+});
