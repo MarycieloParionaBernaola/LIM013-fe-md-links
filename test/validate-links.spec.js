913c05113f697d70c5dfd9bc217aed220800851c
marycielopb <mparionabernaola@gmail.com>
2020-12-09 22:53:50 -0500

    mock fetch added to test validate links - cloes #30

--- test/validate-links.spec.js
+++ test/validate-links.spec.js
@@ -1,68 +1,34 @@
-const path = require('path');
+/* eslint-disable no-undef */
+require('jest-fetch-mock').enableMocks();
 const { validateLinks } = require('../src/validate-links');
+const {
+  mdLinkOk, mdLinkValidatedOk, mdLinkFail, mdLinkValidatedFail,
+} = require('./data');
 
-const cwd = process.cwd();
-const absoluteMdFile1 = path.join(cwd, '\\test-files\\test-links-1.md');
-const absoluteMdFile2 = path.join(cwd, '\\test-files\\test-links-2.md');
-
-const linkStatus200 = [
-  {
-    href: 'https://www.npmjs.com/',
-    path: `${absoluteMdFile2}`,
-    text: 'npm',
-  }];
-
-const linkStatus300 = [
-  {
-    href: 'https://docs.npmjs.com/misc/scripts',
-    path: `${absoluteMdFile1}`,
-    text: 'Npm scripts',
-  }];
-
-const linkStatus400 = [
-  {
-    href: 'https://es.wikipedia.org/wi/Markdown',
-    path: `${absoluteMdFile1}`,
-    text: 'Markdown',
-  }];
-
-const linkStatus500 = [
-  {
-    href: 'https://develoers.google.com/v8/',
-    path: `${absoluteMdFile1}`,
-    text: 'Motor v8',
-  }];
+beforeEach(() => {
+  fetch.resetMocks();
+});
 
 describe('validate all .md links', () => {
-  test('it is a function', () => {
-    expect(typeof validateLinks).toBe('function');
-  });
-  test('it returns status 200', (done) => {
-    validateLinks(linkStatus200)
-      .then(((e) => {
-        expect(e[0].status).toBe(200);
-        done();
+  test('it returns a link with status 200 OK', () => {
+    fetch.mockOnce({ status: 200, statusText: 'OK' });
+    validateLinks(mdLinkOk)
+      .then(((res) => {
+        expect(res).toEqual(mdLinkValidatedOk);
       }));
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+      'https://github.com/merunga/pildora-recursion', { redirect: 'manual' },
+    );
   });
-  test('it returns status 300', (done) => {
-    validateLinks(linkStatus300)
-      .then(((e) => {
-        expect(e[0].status).toBe(301);
-        done();
-      }));
-  });
-  test('it returns status 400', (done) => {
-    validateLinks(linkStatus400)
-      .then(((e) => {
-        expect(e[0].status).toBe(404);
-        done();
-      }));
-  });
-  test('it returns status 500', (done) => {
-    validateLinks(linkStatus500)
-      .then(((e) => {
-        expect(e[0].status).toBe('Error');
-        done();
-      }));
+
+  test('it returns a link with status Error FAIL', () => {
+    fetch.mockRejectOnce(() => validateLinks(mdLinkFail)
+      .then((res) => Promise.reject(res.errorToRaise)));
+    validateLinks(mdLinkFail)
+      .then((res) => {
+        expect(res).toEqual(mdLinkValidatedFail);
+      });
+    expect(fetchMock).toHaveBeenCalledTimes(2);
   });
 });
